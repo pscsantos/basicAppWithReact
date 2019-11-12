5520fcc2fe8777efabf80dee7b0f830ae59fc25a
Paulo Robson Santos Da Costa <F1808111@for.lanlink.com.br>
2019-11-12 11:50:25 -0300

    commit first

---
+++ src/pages/Repository/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Title } from './styles';
+
+// import { Container } from './styles';
+
+export default function Repository() {
+  return (
+    <Title>
+      <h1>Repository</h1>
+    </Title>
+  );
+}
+
